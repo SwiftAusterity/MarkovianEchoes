bb6c0b86476704cc294d9f112d3cd7a3d8c91c92
SwiftAusterity <swiftausterity@gmail.com>
2018-04-21 21:31:46 -0500

    Initial ui for the 3 void pages

--- Echoes/Echoes/wwwroot/js/site.js
+++ Echoes/Echoes/wwwroot/js/site.js
@@ -1,1 +1,82 @@
 ﻿// Write your Javascript code.
+function PositionOutput(text, parent, child, textEffects) {
+    var writingChance = Math.random();
+    var width = 0;
+    var height = 20;
+
+    if (parent == undefined) {
+        parent = $(document);
+    }
+
+    if (child == undefined) {
+        child = $('<div/>').text(text);
+
+        width = (text.length * 3) + 10;
+
+        child.css({
+            'width': width + 'px',
+            'height': '20px',
+            'padding': '5px'
+        });
+
+    } else {
+        width = child.width();
+        height = child.height();
+    }
+
+    if (width >= parent.width()) {
+        width = parent.width() / 3;
+    }
+
+    if (writingChance >= 0.8) {
+        child.css({
+            'writing-mode': 'horizontal-tb',
+            '-webkit-writing-mode': 'horizontal-tb',
+            '-ms-writing-mode': 'horizontal-tb',
+        });
+    } else if (writingChance >= 0.6) {
+        child.width(height);
+        child.height(width);
+
+        child.css({
+            'writing-mode': 'vertical-lr',
+            '-webkit-writing-mode': 'vertical-lr',
+            '-ms-writing-mode': 'vertical-lr',
+        });
+    } else if (writingChance >= 0.4) {
+        child.width(height);
+        child.height(width);
+
+        child.css({
+            'writing-mode': 'vertical-rl',
+            '-webkit-writing-mode': 'vertical-rl',
+            '-ms-writing-mode': 'vertical-rl',
+        });
+    }
+
+    var posx = (Math.random() * (parent.width() - child.width())).toFixed();
+    var posy = (Math.random() * (parent.height() - child.height())).toFixed();
+
+    child.css({
+        'position': 'absolute',
+        'left': posx + 'px',
+        'top': posy + 'px'
+    });
+
+    child.appendTo(parent);
+
+    if (textEffects == undefined) {
+        textEffects = { in: { effect: 'fadeIn' } };
+    }
+
+    child.textillate(textEffects);
+
+    child.mouseover(function () {
+        $(this).hide({
+            duration: 1000,
+            complete: function () {
+                $(this).remove();
+            }
+        });
+    });
+};
