659dd69ed30cb283abe1554caa72f356e747c7e9
SwiftAusterity <swiftausterity@gmail.com>
2018-04-22 19:22:49 -0500

    Functional existence controller, just need the playername now. also hack for mvc might fix later

--- Echoes/Echoes/Controllers/ExistenceController.cs
+++ Echoes/Echoes/Controllers/ExistenceController.cs
@@ -4,10 +4,23 @@ using Microsoft.AspNetCore.Mvc;
 namespace Echoes.Web.Controllers
 {
     [AllowAnonymous]
-    [Route("[controller]/[action]")]
+    [Route("[controller]")]
     public class ExistenceController : Controller
     {
+        [HttpGet("", Name = "Existence_Index")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{place}", Name = "Existence_Place")]
+        public IActionResult Place(string place)
+        {
+            return View();
+        }
+
+        [HttpPost("{place}", Name = "Existence_Place_Input")]
+        public IActionResult Input(string place, string input)
         {
             return View();
         }
